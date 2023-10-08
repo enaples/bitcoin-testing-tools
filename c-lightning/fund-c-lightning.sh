90144fdbd9e9df791ac98510de6b294eaa6792a8
enaples <enaples@users.noreply.github.com>
2023-10-08 15:15:29 +0200

    funding method changed

--- c-lightning/fund-c-lightning.sh
+++ c-lightning/fund-c-lightning.sh
@@ -2,16 +2,20 @@
 set -Eeuo pipefail
 
 # Generate a new receiving address for c-lightning wallet
-address=$(lightning-cli --lightning-dir=/lightningd newaddr | jq '.bech32' -r)
+ADDR=$(lightning-cli --lightning-dir=/lightningd newaddr | jq '.bech32' -r)
 
-# Ask Bitcoin Core to send 10 BTC to the address, using JSON-RPC call
-until bitcoin-cli \
---rpcuser=bitcoin \
---rpcpassword=bitcoin \
---rpcconnect=0.0.0.0:38332 \
---signet \
-sendtoaddress ${address} 1 "funding c-lightning"
-do
-    sleep 1;
-    echo Retrying funding...
-done
+# Bitcoin faucet .onion address
+FAUCET_URL=localhost
+
+if [[ $FAUCET_URL == *"onion"* ]]; then
+    RESPONSE=`curl --silent --socks5-hostname localhost:9050 "${FAUCET_URL}:5050/faucet?address=${ADDR}"`
+else
+    RESPONSE=`curl --silent "${FAUCET_URL}:5000/faucet?address=${ADDR}"`
+fi
+
+
+if [[ $RESPONSE == *"Success"* ]]; then
+    echo "Node funded! Wait for on-chain confirmation."
+else
+    echo "Failed to fund node: $RESPONSE"
+fi
