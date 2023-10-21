9047649f733580074e6d56ab5395c04d699ac63a
enaples <enaples@users.noreply.github.com>
2023-10-21 02:02:10 +0200

    useful management tools added

--- get-cln-strings.sh
+++ get-cln-strings.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
 
 # run-in-node: Run a command inside a docker container, using the bash shell
-function run-in-node () {
-    docker exec "$1" /bin/bash -c "${@:2}"
+
+function get-cln-id(){
+    if [ "$#" -ne 1 ]; then
+        echo "Error: Exactly two arguments are required."
+        return 1
+    fi
+    
+    if [ $1 = 0 ]; then
+        PREFIX=""
+    else
+        PREFIX="-$1"
+    fi
+    
+    CLN_ONION=$(docker exec c-lightning$PREFIX lightning-cli --lightning-dir=/lightningd getinfo | jq -r ".address[].address")
+    CLN_PORT=$(docker exec c-lightning$PREFIX lightning-cli --lightning-dir=/lightningd getinfo | jq -r ".address[].port")
+    CLN_ID=$(docker exec c-lightning$PREFIX lightning-cli --lightning-dir=/lightningd getinfo | jq -r ".id")
+    
+    CLN_CONNECT_STR=$CLN_ID@$CLN_ONION:$CLN_PORT
+    echo $CLN_CONNECT_STR
 }
 
-CLN_ONION=`run-in-node c-lightning 'lightning-cli getinfo | jq -r ".address[].address"'`
-CLN_REST=`run-in-node tor 'cat /var/lib/tor/hidden_service_cln_rest/hostname'`
-CLN_MACAROON=`run-in-node c-lightning 'xxd -ps -u -c 1000 /lightningd/cln-plugins/c-lightning-REST-0.10.5/certs/access.macaroon'`
-CLN_ID=`run-in-node c-lightning 'lightning-cli getinfo | jq -r ".id"'`
+function get-cln-rest(){
+    if [ "$#" -ne 1 ]; then
+        echo "Error: Exactly one argument is required."
+        return 1
+    fi
+    
+    if [ $1 = 0 ]; then
+        PREFIX=""
+    else
+        PREFIX="-$1"
+    fi
+    CLN_REST=$(docker exec tor cat /var/lib/tor/hidden_service_cln${PREFIX}_rest/hostname)
+    CLN_REST_MACAROON=$(docker exec c-lightning$PREFIX xxd -ps -u -c 1000 /lightningd/cln-plugins/c-lightning-REST-0.10.5/certs/access.macaroon)
+    CLN_REST_PORT=$(( 8080 + $1 * 101))
+    REST_STR="c-lightning-rest://http://${CLN_REST}:$CLN_REST_PORT?&macaroon=${CLN_REST_MACAROON}&protocol=http"
+    echo $REST_STR
+}
 
-echo "String for connecting to c-lightning REST API to ZEUS app:"
-REST_STR="c-lightning-rest://http://${CLN_REST}:8080?&macaroon=${CLN_MACAROON}&protocol=http"
-echo $REST_STR
-echo ""
-
-echo "String to give to your peers to connect to your node:"
-ID_STR="${CLN_ID}@${CLN_ONION}:39735"
-echo $ID_STR
-
-# Get QR codes to scan
-curl -so "ZeusControl.png"  "https://api.qrserver.com/v1/create-qr-code/?data=$(jq -s -R -r @uri <<< "$REST_STR")&format=png&size=512x512&margin=10"
-curl -so "peerID.png" "https://api.qrserver.com/v1/create-qr-code/?data=$(jq -s -R -r @uri <<< "$ID_STR")&format=png&size=512x512&margin=10"
+function get-qrs(){
+    if [ "$#" -ne 1 ]; then
+        echo "Error: Exactly two arguments are required."
+        return 1
+    fi
+    ID_STR=$(get-cln-id $1)
+    REST_STR=$(get-cln-rest $1)
+    curl -so "ZeusControl$PREFIX.png"  "https://api.qrserver.com/v1/create-qr-code/?data=$(jq -s -R -r @uri <<< "$REST_STR")&format=png&size=512x512&margin=10"
+    curl -so "peerID$PREFIX.png" "https://api.qrserver.com/v1/create-qr-code/?data=$(jq -s -R -r @uri <<< "$ID_STR")&format=png&size=512x512&margin=10"
+}
