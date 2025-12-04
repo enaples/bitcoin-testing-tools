5775e7392643ab1c6f44e715daa919b8017e34e8
enaples <enaples@users.noreply.github.com>
2025-12-04 16:08:43 +0100

    cln: forgot clnrest is a new feature

--- c-lightning/create-conf.sh
+++ c-lightning/create-conf.sh
@@ -5,7 +5,8 @@ bitcoin-rpcpassword=bitcoin
 bitcoin-rpcconnect=$BTC_HOST
 bitcoin-rpcport=38332
 
-clnrest-port=3010
+# Newr version only
+#clnrest-port=3010
 
 log-level=debug
 log-file=/lightningd/lightningd.log
