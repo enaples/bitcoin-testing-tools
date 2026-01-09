62a2f2870ad0693d84ed02bdcafe5c0bca2301e3
enaples <enaples@users.noreply.github.com>
2026-01-09 15:33:22 +0100

    electrs-blockstream: small config settings changes
    Setting bitcoin node host from env variable on dockerfile

--- blockstream-electrs/wait-for-bitcoind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-
-echo Waiting for bitcoind to mine blocks...
-until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "blockstream-electrs", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://btc_sig_miner:38332/ | jq -e ".result.blocks > 10" > /dev/null 2>&1
-do
-    echo -n "."
-    sleep 1
-done
