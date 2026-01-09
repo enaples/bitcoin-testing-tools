31afe0563357d31b236595c3166a59494146fb63
enaples <enaples@users.noreply.github.com>
2026-01-09 15:35:03 +0100

    electrs-romanz: folder renamed

---
+++ electrs-romanz/wait-for-bitcoind.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+echo Waiting for bitcoind to mine blocks...
+until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "electrs", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://$BTC_HOST:38332/ | jq -e ".result.blocks > 10" > /dev/null 2>&1
+do
+    echo -n "."
+    sleep 1
+done
