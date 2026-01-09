6384c7d1fdd63c55a67af38ffd46d2f8d88f5e95
enaples <enaples@users.noreply.github.com>
2026-01-09 15:35:39 +0100

    electrs-liquid: setting liquid node host from dockerfile

--- electrs-liquid/wait-for-elementsd.sh
+++ electrs-liquid/wait-for-elementsd.sh
@@ -2,7 +2,7 @@
 set -Eeuo pipefail
 
 echo Waiting for liquid to mine blocks...
-until curl --silent --user elements:elements --data-binary '{"jsonrpc": "1.0", "id": "electrs-liquid", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://liquid1:39884/ | jq -e ".result.blocks > 10" > /dev/null 2>&1
+until curl --silent --user elements:elements --data-binary '{"jsonrpc": "1.0", "id": "electrs-liquid", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://$EL_HOST:39884/ | jq -e ".result.blocks > 10" > /dev/null 2>&1
 do
     echo -n "."
     sleep 1
