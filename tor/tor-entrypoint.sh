3399d67ed27c39508e098eceff63cb4436be599b
enaples <enaples@users.noreply.github.com>
2026-01-09 15:39:54 +0100

    tor: changed config file with better naming convention + removed waiting bitcoin because tor must be up and running before bitcoind starts

--- tor/tor-entrypoint.sh
+++ tor/tor-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-source /usr/local/bin/wait-for-bitcoind.sh
-
-source /usr/local/bin/create-conf.sh
+/usr/local/bin/create-conf.sh
 
 exec "$@"
--- tor/wait-for-bitcoind.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -Eeuo pipefail
-
-echo Waiting for bitcoind to mine blocks...
-until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "nginx", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://btc_sig_miner:38332/ | jq -e ".result.blocks > 11" > /dev/null 2>&1
-do
-    echo -n "."
-    sleep 1
-done
