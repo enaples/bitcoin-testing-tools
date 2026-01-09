5031e066475bae66f643d6d16dc601a3210b280e
enaples <enaples@users.noreply.github.com>
2026-01-09 15:33:54 +0100

    cln: btc host set from dockerfile

--- c-lightning/wait-for-bitcoind.sh
+++ c-lightning/wait-for-bitcoind.sh
@@ -2,14 +2,14 @@
 set -Eeuo pipefail
 
 echo Waiting for bitcoind to start...
-until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "cl-node", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://btc_sig_miner:38332/ | jq -e ".result.blocks > 105" > /dev/null 2>&1
+until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "cl-node", "method": "getblockchaininfo", "params": []}' -H 'content-type: text/plain;' http://$BTC_HOST:38332/ | jq -e ".result.blocks > 105" > /dev/null 2>&1
 do
     echo -n "."
     sleep 1
 done
 
 echo Waiting for bitcoind to be funded...
-until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "cl-node", "method": "getbalance", "params": ["*", 1]}' -H 'content-type: text/plain;' http://btc_sig_miner:38332/ | jq -e ".result > 0" > /dev/null 2>&1
+until curl --silent --user bitcoin:bitcoin --data-binary '{"jsonrpc": "1.0", "id": "cl-node", "method": "getbalance", "params": ["*", 1]}' -H 'content-type: text/plain;' http://$BTC_HOST:38332/ | jq -e ".result > 0" > /dev/null 2>&1
 do
     echo -n "."
     sleep 1
