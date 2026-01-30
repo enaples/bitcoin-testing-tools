bfc1912ea5cb0330226d0fe3077a3943bd8bf59e
enaples <enaples@users.noreply.github.com>
2026-01-30 13:42:39 +0100

    misc: fixing LF + updating CLN version

--- tor/create-conf.sh
+++ tor/create-conf.sh
@@ -1,9 +1,10 @@
-cat <<- 'EOF_HEADER' > /etc/tor/torrc
-ControlPort 0.0.0.0:9051
-SOCKSPort 0.0.0.0:9050
+cat <<- EOF_HEADER > "/etc/tor/torrc"
+ControlPort $(hostname -f):9051
+SOCKSPort $(hostname -f):9050
 CookieAuthentication 1
 HashedControlPassword 16:E380FE879AE8380E60C786B3908666C620ECA8D7DAA1AB44B058E3BF64
 CookieAuthFileGroupReadable 1
+CookieAuthFile /var/lib/tor/control_auth_cookie
 EOF_HEADER
 
 # Function to add hidden service if nslookup succeeds
--- tor/tor-entrypoint.sh
+++ tor/tor-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-# Create config file if it doesn't exist
-if [ ! -f "/etc/tor/torrc" ]; then
-    /usr/local/bin/create-conf.sh
-fi
+/usr/local/bin/create-conf.sh
 
 exec "$@"
